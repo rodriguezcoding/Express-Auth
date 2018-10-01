a72e85b4a9a2e21440a5fc8e9041c1c6d9b45f0b
Luis <luis.rodriguezcastro31@gmail.com>
2018-09-30 18:22:38 -0700

    email confirmation service working, still need to implement it

---
+++ services/emailConfirmation.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+const xoauth2 = require("xoauth2");
+const smtp = require("nodemailer-smtp-transport");
+
+const emailConfirmation = (email, hashId) => {
+  const xoAuthGenerate = xoauth2.createXOAuth2Generator({
+    type: "OAuth2",
+    user: "luis.rodriguezcastro31@gmail.com",
+    clientId: process.env.SECRET_KEY_EMAIL_CLIENTID,
+    clientSecret: process.env.SECRET_KEY_EMAIL_SECRETKEY,
+    refreshToken: process.env.SECRET_KEY_EMAIL_REFRESH_TOKEN
+  });
+
+  var smtpTransport = nodemailer.createTransport(
+    smtp({
+      service: "gmail",
+      auth: {
+        xoauth2: xoAuthGenerate
+      }
+    })
+  );
+
+  var mailOptions = {
+    from: "NoReply<authenticate.noreply@gmail.com>",
+    to: email,
+    subject: "Does this works?",
+    generateTextFromHTML: true,
+    html: `<b>Hello welcome to the service, to confirm email please click : http://localhost:3000/api/${hashId}</b>`
+  };
+
+  smtpTransport.sendMail(mailOptions, (error, response) => {
+    if (error) return console.log(error);
+    smtpTransport.close();
+  });
+};
+
+module.exports = emailConfirmation;
