1d61c6ddfb12622049630ae1afc38c5fd0bcb393
Luis <luis.rodriguezcastro31@gmail.com>
2018-10-10 17:26:27 -0700

    CRUD team

--- models/team.js
+++ models/team.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const joi = require("joi");
-const moment = require("moment");
 const _ = require("lodash");
 const teamSchema = new mongoose.Schema({
   ownerID: {
@@ -19,10 +18,6 @@ const teamSchema = new mongoose.Schema({
   teamMembers: [{ type: String, ref: "User" }],
   teamProjects: [{ type: mongoose.Schema.Types.ObjectId, ref: "Project" }],
   date: { type: String }
-});
-teamSchema.pre("save", next => {
-  this.date = moment().format("MMM Do YYYY, h:mm:ss a");
-  next();
 });
 
 const validateTeam = team => {
--- routes/team.js
+++ routes/team.js
@@ -5,10 +5,40 @@ const emailConfirmation = require("../services/emailConfirmation.js");
 const { Team, validateTeam, validateEmail } = require("../models/team.js");
 const authToken = require("../middlewares/authToken.js");
 const bcrypt = require("bcryptjs");
+const moment = require("moment");
 
+//get all teams
 router.get("/teams", authToken, async (req, res) => {
   const teams = await Team.find({ ownerID: req.user._id });
   res.status(200).send(teams);
+});
+
+//
+router.delete("/teams/:id", authToken, async (req, res) => {
+  const team = await Team.findByIdAndRemove(req.params.id);
+  if (!team) return res.status(400).send("Could not delete team");
+
+  res.status(200).send("Team deleted successfully");
+});
+
+//update team name or remove team member "email"
+router.patch("/teams/:id", authToken, async (req, res) => {
+  const team = await Team.findById(req.params.id);
+  if (!team) return res.status(400).send("Could not find this team");
+
+  if (req.body.updateTeam.email) {
+    const { error } = validateEmail(req.body.updateTeam);
+    if (error) return res.status(400).send(error.details[0].message);
+    const filter = team.teamMembers.filter(
+      email => email !== req.body.updateTeam.email
+    );
+    team.teamMembers = filter;
+    await team.save();
+    return res.status(200).send("Team member removed");
+  }
+  team.teamName = req.body.updateTeam.teamName;
+  await team.save();
+  return res.status(200).send("Team name updated!");
 });
 
 //create team
@@ -18,10 +48,11 @@ router.post("/teams", authToken, async (req, res) => {
 
   const team = new Team(req.body.team);
   const hash = await bcrypt.hash(team._id.toString(), 10);
-  const hashedId = hash.replace(/[/\\&;%@+.,]/g, "");
-  team.hashedId = hashedId;
+  hash = hash.replace(/[/\\&;%@+.,]/g, "");
+  team.hashedId = hash;
   team.ownerID = req.user._id;
   team.ownerEmail = req.user.email;
+  team.date = moment().format("MMM Do YYYY, h:mm:ss a");
 
   await team.save();
 
@@ -54,7 +85,7 @@ router.post("/teams/invite/:id", authToken, async (req, res) => {
     close.close();
     return res
       .status(400)
-      .send("There was an error trying to recover account please try again");
+      .send("There was an error trying to send the invite please try again");
   }
   close.close();
   res.status(200).send(`The team invite has been sent`);
